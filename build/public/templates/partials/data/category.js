e4f6ba67dd1fce8f5e0b5fe5bb0a065318e54e97
Javi Moreno <jmoreno.zn@gmail.com>
2018-10-15 00:53:40 +0200

    ojo al dato

---
+++ build/public/templates/partials/data/category.js
@@ -0,0 +1,1 @@
+!function(t){"object"==typeof module&&module.exports?module.exports=t():"function"==typeof define&&define.amd&&define(t)}(function(){function t(t,e,i,o,d){var n=t.__escape;return'data-tid="'+n(i(e&&e.topics&&e.topics.tid))+'" data-index="'+n(i(e&&e.topics&&e.topics.index))+'" data-cid="'+n(i(e&&e.topics&&e.topics.cid))+'" itemprop="itemListElement"'}return t.blocks={},t});
